b143f398c0307d6190cf8964e2ae383ce748922a
YOUR NAME <YOUR@EMAIL.com>
2021-04-28 20:34:36 +0700

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   before_action :find_user, only: [:show ,:edit, :update, :destroy]
   before_action :correct_user,only: [:edit, :update]
 
+  def index
+    @users = User.all.paginate(page: params[:page])
+  end
   
   def show
   end
